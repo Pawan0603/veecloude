79c1d792cf10257010b9abca04c261886b0a4015
Pawan0603 <www.pawanmthakre@gmail.com>
2025-01-12 13:03:08 +0530

    adding social media links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
         <footer className="w-screen px-5 py-8 flex justify-between border-t bg-[#f9f9fa]">
             <p>© VeeCloud.vercel.app</p>
             <div className='flex gap-5'>
-                <Link href={'#'}><Facebook size={20} className='text-gray-400 hover:text-blue-400'/></Link>
-                <Link href={'#'}><Instagram size={20} className='text-gray-400 hover:text-blue-400'/></Link>
-                <Link href={'#'}><Github size={20} className='text-gray-400 hover:text-blue-400'/></Link>
-                <Link href={'#'}><Linkedin size={20} className='text-gray-400 hover:text-blue-400'/></Link>
+                <Link target='_blank' href={'https://www.facebook.com/pawan.thakre.39948'}><Facebook size={20} className='text-gray-400 hover:text-blue-400'/></Link>
+                <Link target='_blank' href={'https://www.instagram.com/pawanthakre_/'}><Instagram size={20} className='text-gray-400 hover:text-blue-400'/></Link>
+                <Link target='_blank' href={'https://github.com/Pawan0603'}><Github size={20} className='text-gray-400 hover:text-blue-400'/></Link>
+                <Link target='_blank' href={'https://www.linkedin.com/in/pawan-thakre-12b99a2b8/'}><Linkedin size={20} className='text-gray-400 hover:text-blue-400'/></Link>
             </div>
         </footer>
     )
